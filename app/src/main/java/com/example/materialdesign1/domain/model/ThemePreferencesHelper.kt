1af43979dd0516ffea4de0dcc0c3894d9936f802
danilfs009@yandex.ru <62214743+danilfs@users.noreply.github.com>
2022-05-23 09:53:11 +0300

    SwitchFragment

---
+++ app/src/main/java/com/example/materialdesign1/domain/model/ThemePreferencesHelper.kt
@@ -0,0 +1,29 @@
+package com.example.materialdesign1.domain.model
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build.VERSION_CODES.R
+
+object ThemePreferencesHelper {
+    object ThemePreferencesHelper {
+
+        private const val THEME_PREFS = "theme_prefs"
+        private const val KEY_THEME_ID = "theme_id"
+
+        private fun themePreferences(context: Context): SharedPreferences =
+            context.getSharedPreferences(
+                THEME_PREFS, Context.MODE_PRIVATE
+            )
+
+        fun readTheme(context: Context, defaultThemeId: Int = R.style.SimpleTheme): Int =
+            themePreferences(context)
+                .getInt(KEY_THEME_ID, defaultThemeId)
+
+        fun saveTheme(context: Context, themeId: Int) =
+            themePreferences(context)
+                .edit()
+                .putInt(KEY_THEME_ID, themeId)
+                .apply()
+
+    }
+}
--- app/src/main/java/com/example/materialdesign1/presentation/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign1/presentation/MainActivity.kt
@@ -1,9 +1,23 @@
 package com.example.materialdesign1.presentation
 
+import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.materialdesign1.R
+import com.example.materialdesign1.domain.model.ThemePreferencesHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setupTheme()
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun setupTheme() {
+        val themeId = ThemePreferencesHelper.readTheme(this)
+        setTheme(themeId)
+    }
+
+}
